f1cfb034496aaa079a9ca507a1fcc8c69dafcc5a
David McCullough <dmccullough488@gmail.com>
2025-02-16 17:09:00 -0600

    added more travels

--- src/data.js
+++ src/data.js
@@ -10,10 +10,22 @@ export default [
     country: "Tennesssee",
     googleMapsLink: "https://maps.app.goo.gl/5BQBeLLtcPbBjYPf8",
     dates: "Jun, 2023",
-    text: "Spent 6 days in Gatlinburg, Tennessee with my family. We started a national parks passport for my son and visited the Great Smoky Mountains National Park, Clingmans Dome observation tower in North Carolina, and the Trail of Tears National Historic Trail. We got to see two black bears. Also got to do the alpine coaster."
+    text: "Spent 6 days in Gatlinburg, Tennessee with my family. I started a national parks passport for my son and visited the Great Smoky Mountains National Park, Clingmans Dome observation tower in North Carolina, Cades Cove and the Trail of Tears National Historic Trail. We got to see two black bears. Also got to do the alpine coaster and went white water rafting."
   },
   {
     id: 2,
+    img: {
+      src: "./src/assets/sonicbloom.png",
+      alt: "Sonic Bloom"
+    },
+    title: "Sonic Bloom",
+    country: "Colorado",
+    googleMapsLink: "https://maps.app.goo.gl/BTZ5gYK9Uzdh9ER88",
+    dates: "Jun, 2015",
+    text: "A 3 day music festival in Rye, Colorado with a group of friends. We camped in the forest and saw performances by STS9, Shpongle, Emancipator, Random Rab, Phutureprimitive, Talib Kweli, Chali 2na, Thriftworks, Vibesquad, Desert Dwellers, Dirtwire, Clozee, Plantrae, Pumpkin, Nico Luminous."
+  },
+  {
+    id: 3,
     img: {
       src: "./src/assets/japan.png",
       alt: "Tokyo national museum"
@@ -22,10 +34,22 @@ export default [
     country: "Japan",
     googleMapsLink: "https://maps.app.goo.gl/uULAkLqBqfzmHkSM7",
     dates: "Feb, 2015",
-    text: "Spent 10 days in Japan with my younger sister & brother. We stayed in Tokyo for 6 days and visited the Tokyo National Museum and got to see the 'Great Wave' by Hokusai. We took a bullet train to Kyoto, and visited the Fushimi Inari-taisha shrine. Then spent 4 days in Niseko, Hokkaido for skiing."
+    text: "Spent 10 days in Japan with my younger sister & brother. We stayed in Tokyo for 6 days and visited the Tokyo National Museum and got to see the 'Great Wave' by Hokusai. We took a bullet train to Kyoto, and visited the Fushimi Inari-taisha shrine. We checked out Akihabara, the worlds largest crosswalk. Then spent 4 days in Niseko, Hokkaido for skiing."
   },
   {
-    id: 3,
+    id: 4,
+    img: {
+      src: "./src/assets/electricF.png",
+      alt: "Electric Forest"
+    },
+    title: "Electric Forest",
+    country: "Michigan",
+    googleMapsLink: "https://maps.app.goo.gl/f4QFyFVKvKXFghYS9",
+    dates: "Jun, 2014",
+    text: "A 4 day music festival in Rothbury, Michigan with a group of friends. We camped in the forest and saw performances by STS9, Cut/Copy, Flying Lotus, Classixx, The Glitch Mob, Zedd, RAC, Umphrey's McGee, Tycho, The String Cheese Incident, Matt & Kim, Zeds Dead, Schoolboy Q. Went to an indoor water park and got a golf cart ride behind the main stage."
+  },
+  {
+    id: 5,
     img: {
       src: "./src/assets/redwood.png",
       alt: "Redwood National Park"
@@ -34,10 +58,10 @@ export default [
     country: "California",
     googleMapsLink: "https://maps.app.goo.gl/gP3rE2cdbE3fW8Ta9",
     dates: "Mar, 2013",
-    text: "Went on a 10 day road trip through Arizona and California with my best friend & younger brother. We visited friends in San Diego, California and went to the San Diego Zoo and. We drove to san Francisco and visited the Golden Gate Bridge, pier 39 aquarium. Then we drove up to Crescent City, CA on highway 101 through the redwood national park where I got to drive my car through a tree."
+    text: "Went on a 10 day road trip through Arizona and California with my best friend Lauren & younger brother. We visited friends in San Diego, California and went to the San Diego Zoo and. We drove to san Francisco and visited the Golden Gate Bridge, pier 39 aquarium. Then we drove up to Crescent City, CA on highway 101 through the redwood national park where I got to drive my car through a tree."
   },
   {
-    id: 4,
+    id: 6,
     img: {
       src: "./src/assets/dali.png",
       alt: "Salvador Dali Museum"
@@ -46,10 +70,10 @@ export default [
     country: "Florida",
     googleMapsLink: "https://maps.app.goo.gl/HuxAFhMSzsrV9sHK6",
     dates: "Feb, 2012",
-    text: "Went on a 10 day road trip through Florida with my best friend & younger brother. We visited the Salvador Dali Museum in St. Petersburg, Florida and saw the 'The Hallucinogenic Toreador' painting. We aslo drove to New Orleans, Louisiana and visited the French Quarter, the Sydney and Walda Besthoff Sculpture Garden, and the United States Mint. I tried alligator for the first time."
+    text: "Went on a 10 day road trip through Florida with my best friend Lauren & younger brother. We visited the Salvador Dali Museum in St. Petersburg, Florida and saw the 'The Hallucinogenic Toreador' painting. We aslo drove to New Orleans, Louisiana and visited the French Quarter, the Sydney and Walda Besthoff Sculpture Garden, and the United States Mint. I tried alligator for the first time."
   },
   {
-    id: 5,
+    id: 7,
     img: {
       src: "./src/assets/giger.png",
       alt: "HR Giger Museum"
@@ -58,10 +82,10 @@ export default [
     country: "Switzerland",
     googleMapsLink: "https://maps.app.goo.gl/heidR1D9gcE2cT3d6",
     dates: " Aug, 2011",
-    text: "Spent 4 days traveling through Switzerland from Germany using the EuroRail pass with my best friend. We visited the HR Giger Museum in Gruyères, Switzerland and saw the original Johnathan Davis aka Korn microphone stand and Alien movie props and artwork prior to the artist's death in 2014. I left my phone in a hotel room in Zurich and had to take a train back to retrieve it :("
+    text: "Spent 4 days traveling through Switzerland from Germany using the EuroRail pass with my best friend Lauren. We visited the HR Giger Museum in Gruyères, Switzerland and saw the original Johnathan Davis aka Korn microphone stand and Alien movie props and artwork prior to the artist's death in 2014. I left my phone in a hotel room in Zurich and had to take a train back to retrieve it :("
   },
   {
-    id: 6,
+    id: 8,
     img: {
       src: "./src/assets/escher.png",
       alt: "M.C Escher museum"
@@ -70,6 +94,6 @@ export default [
     country: "The Netherlands",
     googleMapsLink: "https://maps.app.goo.gl/qRhKWgPG83Vpjxah7",
     dates: "Aug, 2011",
-    text: "Drove through the Netherlands on the autobahn from Germany with my best friend and visited the M.C Escher museum in The Hague. We also visited the Van Gogh Museum in Amsterdam and saw the 'Sunflowers' painting. We visited the Anne Frank House, took a canal tour, and visited the Red Light District. We took a tour of the heineken brewery and saw the 'I amsterdam' sign."
+    text: "Drove through the Netherlands on the autobahn from Germany with my best friend Lauren and visited the M.C Escher museum in The Hague. We also visited the Van Gogh Museum in Amsterdam and saw the 'Sunflowers' painting. We visited the Anne Frank House, took a canal tour, and visited the Red Light District. We took a tour of the heineken brewery and saw the 'I amsterdam' sign."
   }
 ]
