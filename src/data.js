04ab66e88a46f056d58a9b07a47ba74d1e89da2d
David McCullough <dmccullough488@gmail.com>
2025-02-16 01:13:49 -0600

    added images and travel data to my journal

--- src/data.js
+++ src/data.js
@@ -1,38 +1,75 @@
+
 export default [
   {
     id: 1,
     img: {
-      src: "https://scrimba.com/links/travel-journal-japan-image-url",
-      alt: "Mount Fuji"
+      src: "./src/assets/gatlinberg.png",
+      alt: "Gatlinsburg, TN"
     },
-    title: "Mount Fuji",
-    country: "Japan",
-    googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
-    dates: "12 Jan, 2021 - 24 Jan, 2021",
-    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    title: "Gatlinburg",
+    country: "Tennesssee",
+    googleMapsLink: "https://maps.app.goo.gl/5BQBeLLtcPbBjYPf8",
+    dates: "Jun, 2023",
+    text: "Spent 6 days in Gatlinburg, Tennessee with my family. We started a national parks passport for my son and visited the Great Smoky Mountains National Park, Clingmans Dome observation tower in North Carolina, and the Trail of Tears National Historic Trail. We got to see two black bears. Also got to do the alpine coaster."
   },
   {
     id: 2,
     img: {
-      src: "https://scrimba.com/links/travel-journal-australia-image-url",
-      alt: "Sydney Opera House"
+      src: "./src/assets/japan.png",
+      alt: "Tokyo national museum"
     },
-    title: "Sydney Opera House",
-    country: "Australia",
-    googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
-    dates: "27 May, 2021 - 8 Jun, 2021",
-    text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+    title: "Tokyo National Museum",
+    country: "Japan",
+    googleMapsLink: "https://maps.app.goo.gl/uULAkLqBqfzmHkSM7",
+    dates: "Feb, 2015",
+    text: "Spent 10 days in Japan with my younger sister & brother. We stayed in Tokyo for 6 days and visited the Tokyo National Museum and got to see the 'Great Wave' by Hokusai. We took a bullet train to Kyoto, and visited the Fushimi Inari-taisha shrine. Then spent 4 days in Niseko, Hokkaido for skiing."
   },
   {
     id: 3,
     img: {
-      src: "https://scrimba.com/links/travel-journal-norway-image-url",
-      alt: "Geirangerfjord"
+      src: "./src/assets/redwood.png",
+      alt: "Redwood National Park"
     },
-    title: "Geirangerfjord",
-    country: "Norway",
-    googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
-    dates: "01 Oct, 2021 - 18 Nov, 2021",
-    text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    title: "Redwood National Park",
+    country: "California",
+    googleMapsLink: "https://maps.app.goo.gl/gP3rE2cdbE3fW8Ta9",
+    dates: "Mar, 2013",
+    text: "Went on a 10 day road trip through Arizona and California with my best friend & younger brother. We visited friends in San Diego, California and went to the San Diego Zoo and. We drove to san Francisco and visited the Golden Gate Bridge, pier 39 aquarium. Then we drove up to Crescent City, CA on highway 101 through the redwood national park where I got to drive my car through a tree."
   },
+  {
+    id: 4,
+    img: {
+      src: "./src/assets/dali.png",
+      alt: "Salvador Dali Museum"
+    },
+    title: "Savalador Dali Museum",
+    country: "Florida",
+    googleMapsLink: "https://maps.app.goo.gl/HuxAFhMSzsrV9sHK6",
+    dates: "Feb, 2012",
+    text: "Went on a 10 day road trip through Florida with my best friend & younger brother. We visited the Salvador Dali Museum in St. Petersburg, Florida and saw the 'The Hallucinogenic Toreador' painting. We aslo drove to New Orleans, Louisiana and visited the French Quarter, the Sydney and Walda Besthoff Sculpture Garden, and the United States Mint. I tried alligator for the first time."
+  },
+  {
+    id: 5,
+    img: {
+      src: "./src/assets/giger.png",
+      alt: "HR Giger Museum"
+    },
+    title: "HR Giger Museum",
+    country: "Switzerland",
+    googleMapsLink: "https://maps.app.goo.gl/heidR1D9gcE2cT3d6",
+    dates: " Aug, 2011",
+    text: "Spent 4 days traveling through Switzerland from Germany using the EuroRail pass with my best friend. We visited the HR Giger Museum in Gruyères, Switzerland and saw the original Johnathan Davis aka Korn microphone stand and Alien movie props and artwork prior to the artist's death in 2014. I left my phone in a hotel room in Zurich and had to take a train back to retrieve it :("
+  },
+  {
+    id: 6,
+    img: {
+      src: "./src/assets/escher.png",
+      alt: "M.C Escher museum"
+    },
+    title: "M.C Escher Museum",
+    country: "The Netherlands",
+    googleMapsLink: "https://maps.app.goo.gl/qRhKWgPG83Vpjxah7",
+    dates: "Aug, 2011",
+    text: "Drove through the Netherlands on the autobahn from Germany with my best friend and visited the M.C Escher museum in The Hague. We also visited the Van Gogh Museum in Amsterdam and saw the 'Sunflowers' painting. We visited the Anne Frank House, took a canal tour, and visited the Red Light District. We took a tour of the heineken brewery and saw the 'I amsterdam' sign."
+  }
 ]
