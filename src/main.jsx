e8e7836717514c7519ac6340c5aefa393f6b2c6b
David McCullough <dmccullough488@gmail.com>
2025-02-16 18:00:56 -0600

    *

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from "react-dom/client"
-import App from "./App"
+import React from "react"
+import App from "./App.jsx"
+import "./index.css"
 
 
 
 const root = createRoot(document.getElementById("root"))
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
