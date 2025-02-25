e869b56a5482cd1c7eacdc79e44ff1b510712ef6
David McCullough <dmccullough488@gmail.com>
2025-02-24 18:43:27 -0600

    added categories to data array to add sort function

--- src/data.js
+++ src/data.js
@@ -20,7 +20,8 @@ export default [
     country: "Tennesssee",
     googleMapsLink: "https://maps.app.goo.gl/5BQBeLLtcPbBjYPf8",
     dates: "Jun, 2023",
-    text: "Spent 6 days in Gatlinburg, Tennessee with my family. I started a national parks passport for my son and visited the Great Smoky Mountains National Park, Clingmans Dome observation tower in North Carolina, Cades Cove and the Trail of Tears National Historic Trail. We got to see two black bears. Also got to do the alpine coaster and went white water rafting."
+    text: "Spent 6 days in Gatlinburg, Tennessee with my family. I started a national parks passport for my son and visited the Great Smoky Mountains National Park, Clingmans Dome observation tower in North Carolina, Cades Cove and the Trail of Tears National Historic Trail. We got to see two black bears. Also got to do the alpine coaster and went white water rafting.",
+    category: "national-parks"
   },
   {
     id: 2,
@@ -33,6 +34,7 @@ export default [
     googleMapsLink: "https://maps.app.goo.gl/Z19Sn1Kgha3FEBKF7",
     dates: "Jul, 2019",
     text: "Spent 6 days in an A-frame cabin in Winter Park, Colorado with my family. We vistied the Rocky Mountain National Park, The Royal Gorge Bridge, and the Glenwood Caverns Adventure Park. We also saw a moose in the driveway of our cabin.",
+    category: "national-parks"
   },
   {
     id: 3,
@@ -44,7 +46,8 @@ export default [
     country: "Colorado",
     googleMapsLink: "https://maps.app.goo.gl/BTZ5gYK9Uzdh9ER88",
     dates: "Jun, 2015",
-    text: "A 3 day music festival in Rye, Colorado I attended with a group of friends. We camped in the forest and saw performances by STS9, Shpongle, Emancipator, Random Rab, Phutureprimitive, Talib Kweli, Chali 2na, Thriftworks, Vibesquad, Desert Dwellers, Dirtwire, Clozee, Plantrae, Pumpkin, Nico Luminous."
+    text: "A 3 day music festival in Rye, Colorado I attended with a group of friends. We camped in the forest and saw performances by STS9, Shpongle, Emancipator, Random Rab, Phutureprimitive, Talib Kweli, Chali 2na, Thriftworks, Vibesquad, Desert Dwellers, Dirtwire, Clozee, Plantrae, Pumpkin, Nico Luminous.",
+    category: "concerts"
   },
   {
     id: 4,
@@ -56,7 +59,8 @@ export default [
     country: "Japan",
     googleMapsLink: "https://maps.app.goo.gl/uULAkLqBqfzmHkSM7",
     dates: "Feb, 2015",
-    text: "Spent 10 days in Japan with my younger sister & brother. We stayed in a ryokan (traditional Japanese house) in Tokyo and visited the Tokyo National Museum and got to see the 'Great Wave' by Hokusai. We took a bullet train to Kyoto, and visited the Fushimi Inari-taisha shrine. We checked out Akihabara, the worlds largest crosswalk and the Harajuku district in Shibuya, Japan. To wrap the trip up we spent 4 days in Niseko, Hokkaido for skiing."
+    text: "Spent 10 days in Japan with my younger sister & brother. We stayed in a ryokan (traditional Japanese house) in Tokyo and visited the Tokyo National Museum and got to see the 'Great Wave' by Hokusai. We took a bullet train to Kyoto, and visited the Fushimi Inari-taisha shrine. We checked out Akihabara, the worlds largest crosswalk and the Harajuku district in Shibuya, Japan. To wrap the trip up we spent 4 days in Niseko, Hokkaido for skiing.",
+    category: ["museums", "international"]
   },
   {
     id: 5,
@@ -68,7 +72,8 @@ export default [
     country: "Michigan",
     googleMapsLink: "https://maps.app.goo.gl/f4QFyFVKvKXFghYS9",
     dates: "Jun, 2014",
-    text: "A 4 day music festival in Rothbury, Michigan I attended with a group of friends. We camped in the forest and saw performances by STS9, Cut/Copy, Flying Lotus, Classixx, The Glitch Mob, Zedd, RAC, Umphrey's McGee, Tycho, The String Cheese Incident, Matt & Kim, Zeds Dead, and Schoolboy Q. Went to an indoor water park and got a golf cart ride behind the main stage."
+    text: "A 4 day music festival in Rothbury, Michigan I attended with a group of friends. We camped in the forest and saw performances by STS9, Cut/Copy, Flying Lotus, Classixx, The Glitch Mob, Zedd, RAC, Umphrey's McGee, Tycho, The String Cheese Incident, Matt & Kim, Zeds Dead, and Schoolboy Q. Went to an indoor water park and got a golf cart ride behind the main stage.",
+    category: "concerts"
   },
   {
     id: 6,
@@ -80,7 +85,8 @@ export default [
     country: "California",
     googleMapsLink: "https://maps.app.goo.gl/gP3rE2cdbE3fW8Ta9",
     dates: "Mar, 2013",
-    text: "Went on a 10 day road trip through Arizona and California with my best friend & younger brother. We visited friends in San Diego, California and went to the San Diego Zoo. Then we drove to San Francisco and visited the Golden Gate Bridge, pier 39 aquarium, and the USS Jeremiah O'Brien. Then we drove up to Crescent City, CA on highway 101 through the redwood national park where I got to drive my car through a tree."
+    text: "Went on a 10 day road trip through Arizona and California with my best friend & younger brother. We visited friends in San Diego, California and went to the San Diego Zoo. Then we drove to San Francisco and visited the Golden Gate Bridge, pier 39 aquarium, and the USS Jeremiah O'Brien. Then we drove up to Crescent City, CA on highway 101 through the redwood national park where I got to drive my car through a tree.",
+    category: "national-parks"
   },
   {
     id: 7,
@@ -92,7 +98,8 @@ export default [
     country: "Florida",
     googleMapsLink: "https://maps.app.goo.gl/HuxAFhMSzsrV9sHK6",
     dates: "Feb, 2012",
-    text: "Went on a 10 day road trip through Florida and Louisiana with my best friend & younger brother. We visited the Salvador Dali Museum in St. Petersburg, Florida and saw the 'The Hallucinogenic Toreador' painting. We aslo drove to New Orleans, Louisiana and visited the French Quarter, the Sydney and Walda Besthoff Sculpture Garden, and the United States Mint. We also took a tour of a rum distillery and went on a ghost tour. I even tried alligator for the first time."
+    text: "Went on a 10 day road trip through Florida and Louisiana with my best friend & younger brother. We visited the Salvador Dali Museum in St. Petersburg, Florida and saw the 'The Hallucinogenic Toreador' painting. We aslo drove to New Orleans, Louisiana and visited the French Quarter, the Sydney and Walda Besthoff Sculpture Garden, and the United States Mint. We also took a tour of a rum distillery and went on a ghost tour. I even tried alligator for the first time.",
+    category: "museums"
   },
   {
     id: 8,
@@ -104,7 +111,8 @@ export default [
     country: "Switzerland",
     googleMapsLink: "https://maps.app.goo.gl/heidR1D9gcE2cT3d6",
     dates: " Aug, 2011",
-    text: "Spent 4 days traveling through Switzerland from Germany using the EuroRail pass with my best friend Lauren. We visited the HR Giger Museum in Gruyères, Switzerland and saw the original Johnathan Davis aka Korn microphone stand and Alien movie props and artwork prior to the artist's death in 2014. I left my phone in a hotel room in Zurich and had to take a train back to retrieve it :("
+    text: "Spent 4 days traveling through Switzerland from Germany using the EuroRail pass with my best friend Lauren. We visited the HR Giger Museum in Gruyères, Switzerland and saw the original Johnathan Davis aka Korn microphone stand and Alien movie props and artwork prior to the artist's death in 2014. I left my phone in a hotel room in Zurich and had to take a train back to retrieve it :(",
+    category: ["museums", "international"]
   },
   {
     id: 9,
@@ -116,7 +124,8 @@ export default [
     country: "The Netherlands",
     googleMapsLink: "https://maps.app.goo.gl/qRhKWgPG83Vpjxah7",
     dates: "Aug, 2011",
-    text: "Drove through the Netherlands on the autobahn from Germany with my best friend Lauren and visited the M.C Escher museum in The Hague. We also visited the Van Gogh Museum in Amsterdam and saw the 'Sunflowers' painting. We visited the Anne Frank House, took a canal tour, and visited the Red Light District, toured of the heineken brewery, saw the 'I amsterdam' sign and went to the torture museum."
+    text: "Drove through the Netherlands on the autobahn from Germany with my best friend Lauren and visited the M.C Escher museum in The Hague. We also visited the Van Gogh Museum in Amsterdam and saw the 'Sunflowers' painting. We visited the Anne Frank House, took a canal tour, and visited the Red Light District, toured of the heineken brewery, saw the 'I amsterdam' sign and went to a torture museum.",
+    category: ["museums", "international"]
   },
   {
     id: 10,
@@ -128,6 +137,7 @@ export default [
     country: "Colorado",
     googleMapsLink: "https://maps.app.goo.gl/c9941cQLNRReQJ839",
     dates: "Jun, 2009-2011",
-    text: "A one night event I attended for 3 years in a row with a group of friends in Denver, Colorado. We saw performances by An-ten-nae, Infected Mushroom, The Crystal Method, Paul Van Dyk, Dirty Phonics, 12th Planet, Kill the Noise, Benny Benassi, Kaskade, Ferry Corsten, Datsik, Axwell, Manufactured Superstars, ATB, Sander Van Doorn, Feed Me, Nosia, Downlink, Break Science, and many more."
+    text: "A one night event I attended for 3 years in a row with a group of friends in Denver, Colorado. We saw performances by An-ten-nae, Infected Mushroom, The Crystal Method, Paul Van Dyk, Dirty Phonics, 12th Planet, Kill the Noise, Benny Benassi, Kaskade, Ferry Corsten, Datsik, Axwell, Manufactured Superstars, ATB, Sander Van Doorn, Feed Me, Nosia, Downlink, Break Science, and many more.",
+    category: "concerts"
   }
 ]
